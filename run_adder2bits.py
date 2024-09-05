2c820c9a0152acc1faff6c6829e6d9c18196b63b
pedrao05 <142225824+pedrao05@users.noreply.github.com>
2024-09-05 21:26:05 +0000

    Somador

--- run_adder2bits.py
+++ run_adder2bits.py
@@ -16,7 +16,7 @@ def runAdder2bits():
     s = ConcatSignal(*reversed(s_))
     carry = Signal(bool(0))
 
-    dut = adderModbv(x_, y_, s_, carry)
+    dut = adder2bits(x_, y_, s_, carry)
 
     @instance
     def stimulus():
--- ula_modules.py
+++ ula_modules.py
@@ -11,22 +11,40 @@ from myhdl import *
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
-        pass
+        soma.next = a ^ b
+        carry.next = a & b 
 
     return instances()
 
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    # s1 = Signal(bool(0))
+    # s2 = Signal(bool(0)) 
+    # s3 = Signal(bool(0))
+    s = [Signal(bool(0)) for i in range(3)]
+
+    half_1 = halfAdder(a, b, s[0], s[1]) 
+    half_2 = halfAdder(c, s[0], soma, s[2])
+
     @always_comb
     def comb():
-        pass
+        # soma.next = a ^ b ^ c
+        # carry.next = (a & b) | (b & c) | (a & c)
+
+        carry.next = s[1] | s[2] 
 
     return instances()
 
 
 @block
 def adder2bits(x, y, soma, carry):
+    carry1 = Signal(bool(0))
+
+    ha = halfAdder(x[0], y[0], soma[0], carry1)
+
+    fa = fullAdder(x[1], y[1], carry1, soma[1], carry)
+    
     return instances()
 
 
